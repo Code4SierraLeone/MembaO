3dc2484b41fe366b36af8a69813830764a47e97d
alkawamara <alkawamara@gmail.com>
2017-02-09 23:09:53 +0300

    Initial commit of custom framework

---
+++ lib/config.ini.php
@@ -0,0 +1,35 @@
+<?php 
+	/** 
+	* Configuration
+
+   * @package Membao
+   * @author Alan Kawamara
+   * @copyright 2017
+	*/
+ 
+	 if (!defined("_VALID_PHP")) 
+     die('Direct access to this location is not allowed.');
+ 
+	/** 
+	* Database Constants - these constants refer to 
+	* the database configuration settings. 
+	*/
+	 define('DB_SERVER', ''); 
+	 define('DB_USER', ''); 
+	 define('DB_PASS', ''); 
+	 define('DB_DATABASE', '');
+ 
+	/** 
+	* Show MySql Errors. 
+	* Not recomended for live site. true/false 
+	*/
+	 define('DEBUG', false);
+ 
+	/** 
+	* Cookie Constants - these are the parameters 
+	* to the setcookie function call, change them 
+	* if necessary to fit your website 
+	*/
+	 define('COOKIE_EXPIRE', 60 * 60 * 24 * 60); 
+	 define('COOKIE_PATH', '/');
+?>
