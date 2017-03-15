4951649dcc44a694124a386b233f701b427d9a9d
alkawamara <alan@corporateart-ea.com>
2017-03-15 08:25:43 +0300

    Added frontend bills page

---
+++ bills.php
@@ -0,0 +1,21 @@
+<?php
+  /**
+   * Index
+   *
+   * @package Membao
+   * @author Alan Kawamara
+   * @copyright 2017
+   */
+  define("_VALID_PHP", true);
+  require_once("init.php");
+    
+  if (isset($_GET['billname'])):
+    $billrow = $bill->renderBill();    
+    if(!$billrow):
+      //redirect_to(SITEURL . '/404.php');
+    endif;
+  else:
+    $allbills = $bill->getBills();
+  endif;
+?>
+<?php require_once (THEMEDIR . "/bills.tpl.php");?>
