591bcd380165172b3cef9c324cf73d2408861c34
alkawamara <alan@corporateart-ea.com>
2017-03-16 08:27:51 +0300

    Updated frontend bills template

--- themes/camembao/bills.tpl.php
+++ themes/camembao/bills.tpl.php
@@ -12,6 +12,8 @@
 ?>
 <?php include("header.tpl.php");?>
 
+<div class="page-container">
+
 <?php if (isset($allbills)):?>
 
 <div class="corporato-grid">
@@ -39,6 +41,15 @@
       <?php endif;?>
       </div>
     </div>
+
+    <div class="screen-30 tablet-40 phone-100">
+      <div class="padded-30">
+        <h4>What is a bill</h4>
+        <p>A Bill is a proposal for a new law, or a proposal to change an existing law that is presented for debate before Parliament.</p>
+        <p>Bills are introduced in Parliament as Public Bills by either sitting MPs (Private Member's Bill) or representatives of Government (Government Bill) for examination, discussion and amendment.</p>
+      </div>
+    </div>
+
   </div>
 
   <div class="corporato divider"></div>
@@ -53,19 +64,22 @@
 <div class="corporato-grid">  
   <div class="columns horizontal-gutters">
     <div class="screen-70 tablet-60 phone-100">
-      <div class="corporato basic message">
-        <h2><?php echo $billrow->title;?></h2>
-        <div><?php echo cleanOut($billrow->description);?></div>
-      </div>
+      <h2><?php echo $billrow->title;?></h2>
+      <div>This is a <strong><?php echo getBillType($billrow->bill_type);?></strong> introduced on <strong><?php echo Filter::dodate("short_date", $billrow->date_introduced);?></strong></div>
+      <div class="corporato divider"></div>
+      <div><?php echo cleanOut($billrow->description);?></div>
     </div>
       
     <div class="screen-30 tablet-40 phone-100">
-      <div class="padded-60">
-        <button type="submitfollow" class="corporato large black button">Follow this bill for email updates</button>
+      <div class="padded-30 pull-right">
+        <button type="submitfollow" class="corporato large black button">Follow this bill for updates</button>
       </div>
     </div>
   </div>
 </div>  
 
 <?php endif;?>
+
+</div>
+
 <?php include("footer.tpl.php");?>
