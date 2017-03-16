5d404b750d264af6c9e45429cc8b3b3f887dabbd
Alan Kawamara <alan@corporateart-ea.com>
2017-03-16 17:07:35 +0300

    Updated frontend committees page

---
+++ committees.php
@@ -0,0 +1,22 @@
+<?php
+  /**
+   * Committees
+   *
+   * @package Membao
+   * @author Alan Kawamara
+   * @copyright 2017
+   */
+  define("_VALID_PHP", true);
+  require_once("init.php");
+    
+  if (isset($_GET['committeename'])):
+    $committeerow = $committee->renderCommittee();  
+    $committeemembersrow = $committee->getCommitteeMembers($committeerow->id); 
+    if(!$committeerow):
+      redirect_to(SITEURL . '/404.php');
+    endif;
+  else:
+    $allcommittees = $committee->getCommittees();
+  endif;
+?>
+<?php require_once (THEMEDIR . "/committees.tpl.php");?>
