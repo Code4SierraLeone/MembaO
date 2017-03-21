2245a25a7fd2d4a59d7abcb716403a148d478f43
alkawamara <alan@corporateart-ea.com>
2017-03-21 08:16:52 +0300

    Updated frontend home search template

--- themes/camembao/home_search.tpl.php
+++ themes/camembao/home_search.tpl.php
@@ -10,36 +10,37 @@
 ?>
 <div class="corporato secondary segment">
   <h4 class="corporato shadow header">Find Parliamentary Data</h4>
-  <div class="corporato divider"></div>
-  <div class="corporato small form">
-     <form id="corporato_form_s" name="corporato_form_s" action="<?php echo SITEURL;?>/search/" method="get">
-                   
-      <div class="field">          
-        <button type="submitsearch" class="corporato large black button">Find political candidates</button>
+  <div class="corporato form">
+     <form id="corporato_form_s" name="corporato_form_s" action="<?php echo SITEURL;?>/search/" method="get">                        
+
+      <div class="field">
+        <div class="corporato action input"> 
+          <input name="search-home" type="text" placeholder="Search the Memba O database" id="seach-home" />
+          <a type="submitsearch" class="corporato membao button">Find</a> </div>
       </div>
       
     </form>
 
-    <div class="corporato-content home-lower">
+    <div class="home-lower">
       <div class="four columns horizontal-gutters">
         <div class="screen-25 tablet-25 phone-100 padded10">
-          <div class="stat-title"><?php echo $attendanceperc;?><span class="smallest">%</span></div>
-          <div class="stat-desc">Attendance of MPs in this parliament</div>
+          <div class="stat-title"><span class="count"><?php echo $attendanceperc;?></span><span class="smallest pc">%</span></div>
+          <div class="stat-desc">The average attendance for Parliamentary sittings</div>
         </div>
     
         <div class="screen-25 tablet-25 phone-100 padded10">
-          <div class="stat-title"><?php echo $attendanceperc;?><span class="smallest">%</span></div>
-          <div class="stat-desc">Number of bills tabled by this parliament</div>
+          <div class="stat-title"><span class="count"><?php echo $totalbills;?></span><span class="smallest"></span></div>
+          <div class="stat-desc">Number of bills introduced during this parliament</div>
         </div>
     
         <div class="screen-25 tablet-25 phone-100 padded10">
-          <div class="stat-title"><?php echo $attendanceperc;?><span class="smallest">%</span></div>
-          <div class="stat-desc">Percentage of bills passed by this parliament</div>
+          <div class="stat-title"><span class="count"><?php echo $totalbillspassed;?></span><span class="smallest"></span></div>
+          <div class="stat-desc">Number of bills passed into law by this parliament</div>
         </div>
       
         <div class="screen-25 tablet-25 phone-100 padded10">
-          <div class="stat-title"><?php echo $attendanceperc;?><span class="smallest">%</span></div>
-          <div class="stat-desc">Percentage of independents <span class="small">/ presidential & parliamentary</span></div>
+          <div class="stat-title"><span class="count"><?php echo $totalcommitteemeetings;?></span><span class="smallest"></span></div>
+          <div class="stat-desc">Number of Committee hearings have been scheduled</div>
         </div>
 
       </div>
@@ -47,3 +48,4 @@
 
   </div>
 </div>
+
